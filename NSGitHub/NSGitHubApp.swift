0a4ffb82ec8f2acff15e62623c3c7c74d530a5fb
Sina Rabiei <rabie_sina@yahoo.com>
2023-04-27 20:40:37 +0200

    Initial commit

---
+++ NSGitHub/NSGitHubApp.swift
@@ -0,0 +1,17 @@
+//
+//  NSGitHubApp.swift
+//  NSGitHub
+//
+//  Created by Sina Rabiei on 4/27/23.
+//
+
+import SwiftUI
+
+@main
+struct NSGitHubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
