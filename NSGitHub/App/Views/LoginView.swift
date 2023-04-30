26eb598dedec96325451f483f0d08f60fce36247
Sina Rabiei <sina.rabiei@sectoralarm.com>
2023-04-30 13:37:55 +0200

    Create LoginView.swift

---
+++ NSGitHub/App/Views/LoginView.swift
@@ -0,0 +1,49 @@
+//
+//  LoginView.swift
+//  NSGitHub
+//
+//  Created by Sina Rabiei on 4/30/23.
+//
+
+import SwiftUI
+
+struct LoginView: View {
+    var body: some View {
+        ZStack {
+            Image("GitHubLogo")
+                .resizable()
+                .renderingMode(.template)
+                .foregroundColor(.primary)
+                .frame(width: 90, height: 90)
+                .padding(.bottom, 50)
+            
+            VStack {
+                Spacer()
+                
+                Button {
+                    
+                } label: {
+                    Text("Sign in with GitHub")
+                        .foregroundColor(.primary)
+                        .padding(.vertical, 5)
+                        .padding(.horizontal, 20)
+                }
+                .buttonStyle(.bordered)
+                .buttonBorderShape(.capsule)
+                
+                Text("By signing in you accept our [Term of use](https://docs.github.com/en/site-policy/github-terms/github-terms-of-service) and\n[Privacy policy](https://docs.github.com/en/site-policy/github-terms/github-terms-of-service).")
+                    .font(.caption)
+                    .foregroundColor(.secondary)
+                    .multilineTextAlignment(.center)
+                    .padding(.top, 10)
+            }
+            .padding(.bottom, 50)
+        }
+    }
+}
+
+struct LoginView_Previews: PreviewProvider {
+    static var previews: some View {
+        LoginView()
+    }
+}
