ab4a2056f214e9ad6868fc48f070ae7ca0a76f98
Sina Rabiei <sina.rabiei@sectoralarm.com>
2023-04-30 20:55:30 +0200

    Create LoginEndpoint.swift

---
+++ NSGitHub/Core/Endpoints/LoginEndpoint.swift
@@ -0,0 +1,36 @@
+//
+//  UserEndpoint.swift
+//  NSGitHub
+//
+//  Created by Sina Rabiei on 4/30/23.
+//
+
+import Foundation
+
+enum LoginEndpoint {
+    case login(id: String, uri: String)
+    case token(id: String, secret: String, code: String, uri: String)
+}
+
+extension LoginEndpoint: Endpoint {
+    var baseURL: String {
+        return "https://github.com/login/oauth/"
+    }
+    
+    var path: String {
+        switch self {
+        case .login(let id, let uri):
+            return "authorize?client_id=\(id)&redirect_uri=\(uri)"
+        case .token(let id, let secret, let code, let uri):
+            return "access_token?client_id=\(id)&client_secret=\(secret)&code=\(code)&redirect_uri=\(uri)"
+        }
+    }
+    
+    var header: [String: String]? {
+        return ["Accept": HTTPContentType.applicationJSON]
+    }
+    
+    var urlParams: [URLQueryItem]? {
+        return nil
+    }
+}
