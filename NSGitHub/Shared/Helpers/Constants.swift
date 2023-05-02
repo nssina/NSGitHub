6f808c6d1062a5e504be30ec0765fa80856cbd44
Sina Rabiei <rabie_sina@yahoo.com>
2023-05-03 00:18:15 +0200

    Update constants with new values

--- NSGitHub/Shared/Helpers/Constants.swift
+++ NSGitHub/Shared/Helpers/Constants.swift
@@ -26,16 +26,24 @@ struct CallbackScheme {
 
 // MARK: - SF Symbols
 struct SFSymbols {
-    static let checkmark = "checkmark"
-    static let sort = "line.3.horizontal.decrease.circle"
-    static let person = "person.crop.circle"
-    static let star = "star"
-    static let starFill = "star.fill"
-    static let hammerFill = "hammer.fill"
+    static let checkmark    = "checkmark"
+    static let sort         = "line.3.horizontal.decrease.circle"
+    static let person       = "person.crop.circle"
+    static let star         = "star"
+    static let starFill     = "star.fill"
+    static let hammerFill   = "hammer.fill"
+    static let fork         = "tuningfork"
+    static let share        = "square.and.arrow.up"
+    static let lock         = "lock"
 }
 
 // MARK: - Lottie
 struct Lottie {
+    static let githubLight = "github_light"
+    static let githubDark  = "github_dark"
+    
     static let catLight = "cat_head_light"
-    static let catDark = "cat_head_dark"
+    static let catDark  = "cat_head_dark"
+    
+    static let error404 = "404_error"
 }
