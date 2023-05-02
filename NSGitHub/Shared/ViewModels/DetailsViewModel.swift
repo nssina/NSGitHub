abac2999a49b63e9491943d2865de5509efccd73
Sina Rabiei <rabie_sina@yahoo.com>
2023-05-03 00:17:21 +0200

    Create details view model

---
+++ NSGitHub/Shared/ViewModels/DetailsViewModel.swift
@@ -0,0 +1,53 @@
+//
+//  DetailsViewModel.swift
+//  NSGitHub
+//
+//  Created by Sina Rabiei on 5/2/23.
+//
+
+import Foundation
+
+final class DetailsViewModel: ObservableObject {
+    
+    @Published private(set) var readme: String
+    @Published private(set) var isLoading: Bool
+    
+    private var timer: Timer?
+    private var dismissLoadingAfterSeconds: CGFloat = 1.0
+    
+    init() {
+        isLoading = true
+        readme = ""
+    }
+    
+    func getStringFromURL(endpoint: DetailsEndpoint) async {
+        guard let url = URL(string: endpoint.baseURL + endpoint.path) else { return }
+        
+        do {
+            let contents = try String(contentsOf: url)
+            DispatchQueue.main.async { [weak self] in
+                guard let self = self else { return }
+                self.readme = contents
+                self.stopLoading()
+            }
+        } catch {
+            DispatchQueue.main.async { [weak self] in
+                guard let self = self else { return }
+                self.readme = ""
+                self.stopLoading()
+            }
+            
+            #if DEBUG
+            print(error)
+            #endif
+        }
+    }
+    
+    private func stopLoading() {
+        timer?.invalidate()
+        timer = Timer.scheduledTimer(withTimeInterval: dismissLoadingAfterSeconds, repeats: false) { [weak self] _ in
+            guard let self = self else { return }
+            self.isLoading = false
+        }
+    }
+}
